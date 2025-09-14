bcf7cd41e5d58b01edd26a7fb4b24347a26eec02
CodeX1315 <deepakmahade777@gmail.com>
2025-09-14 17:44:06 +0530

    card component

---
+++ src/component/MovieCard/index.jsx
@@ -0,0 +1,55 @@
+
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import Grid from '@mui/material/Grid';
+import Chip from '@mui/material/Chip';
+
+export default function MovieCard({movie}) {
+
+  const { title, images: { jpg: { image_url } }, season, source, score, duration } = movie;
+  const { genres } = movie;
+
+const genreNames = genres.map(({ name }) => name).join(", ");
+
+console.log(genreNames); // ["Adventure", "Drama", "Fantasy"]
+
+  return (
+    <Grid item xs={12} >
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 200 }}
+        image={ image_url }
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          Season: {season}<br />
+          Source: {source}
+        </Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          Genre: <Chip label={genreNames} />
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          Duration: {duration}
+        </Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          Rating: {score}
+        </Typography>
+      </CardActions>
+    </Card>
+    </Grid>
+  );
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,6 +2,10 @@ import { useEffect } from 'react';
 import Navbar from '../../component/Navbar';
 import { useDispatch, useSelector} from 'react-redux';
 import { getMovies } from '../../api/movies';
+import MovieCard from '../../component/MovieCard';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+
 
 export const Home = () => {
 
@@ -11,10 +15,19 @@ export const Home = () => {
 
     useEffect(() => {
         dispatch(getMovies());
-    }, [])
+    })
     return (
         <>
             <Navbar />
+            <Box  sx={{ flexGrow: 1, margin: 6, gap: 3,  }}>
+                <Grid container spacing={2}>
+                {
+                    // movies?.length > 0 && movies.map(movie => <MovieCard key={movie.id} movie={movie}/>)
+                    movies?.length > 0 && movies.map(movie => <MovieCard key={movie.id} movie={movie}/>)
+                }
+                </Grid>
+            </Box>
+
         </>
     )
 }
